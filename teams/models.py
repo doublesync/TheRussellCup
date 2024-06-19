a154444ee2f347411fe590395042ee1b8836106e
christopher.carlton <chriscarlton368@gmail.com>
2024-06-19 07:39:33 -0400

    Changed formatting.

--- teams/models.py
+++ teams/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Team(models.Model):
+
     # Defined fields
     name = models.CharField(max_length=32)
     city = models.CharField(max_length=32)

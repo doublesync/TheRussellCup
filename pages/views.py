7f67dda34db2f57d8d6fcaa7e77433c13320617e
christopher.carlton <chriscarlton368@gmail.com>
2024-06-22 02:57:10 -0400

    Added login requirement for home page.

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 # Django imports
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Local imports
@@ -6,6 +7,7 @@ from accounts.models import CustomUser
 from players.models import Player
 
 
+@login_required
 def home(request):
     user_players = Player.objects.filter(user=request.user)
     return render(request, "pages/home.html", {"player_files": user_players})

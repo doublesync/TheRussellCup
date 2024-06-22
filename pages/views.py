acea6a2e2ffab58beff971045305d716c27e7b32
christopher.carlton <chriscarlton368@gmail.com>
2024-06-22 02:37:13 -0400

    Added player page.

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
+# Django imports
 from django.shortcuts import render
+
+# Local imports
+from accounts.models import CustomUser
+from players.models import Player
 
 
 def home(request):
-    return render(request, "pages/home.html")
+    user_players = Player.objects.filter(user=request.user)
+    return render(request, "pages/home.html", {"player_files": user_players})

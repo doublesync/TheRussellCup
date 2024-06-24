cde98715f7dcbeb91dba3fb9dca2877f1ba91a3d
christopher.carlton <chriscarlton368@gmail.com>
2024-06-23 23:14:39 -0400

    Raised anomaly chance to 5%.

--- simulation/scripts/anomaly.py
+++ simulation/scripts/anomaly.py
@@ -29,7 +29,7 @@ def height_anomaly(player):
 
 # Changes the chance of an anomaly occurring
 # Each anomaly points to a function that applies the anomaly
-anomaly_chances = 1.00
+anomaly_chances = 0.05
 anomalies = {
     "Height": height_anomaly,
 }

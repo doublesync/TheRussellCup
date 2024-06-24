d76f7e1486585c2d91071dae62cfe2556a384434
christopher.carlton <chriscarlton368@gmail.com>
2024-06-23 23:14:20 -0400

    Updated anomaly effects.

--- simulation/scripts/anomaly.py
+++ simulation/scripts/anomaly.py
@@ -8,15 +8,28 @@ import simulation.config as config
 
 # These are anomaly methods that are called when an anomaly is received
 def height_anomaly(player):
-    player.height += 3
+    # Initialize the anomaly effects
     player.anomaly = True
-    player.user.sp += config["CONFIG_PLAYER"]["SP_ANOMALY_BONUS"]
+    player.height += random.randint(1, 3)
+    player.user.sp += config.CONFIG_PLAYER["SP_ANOMALY_BONUS"]
+    # Set the random boost values
+    speed_boost = random.randint(5, 10)
+    acceleration_boost = random.randint(5, 10)
+    strength_boost = random.randint(5, 10)
+    vertical_boost = random.randint(5, 10)
+    # Boost the player's attributes (capped at 99)
+    player.attributes["Speed"] = min(99, player.attributes["Speed"] + speed_boost)
+    player.attributes["Speed with Ball"] = player.attributes["Speed"]
+    player.attributes["Acceleration"] = min(99, player.attributes["Acceleration"] + acceleration_boost)
+    player.attributes["Strength"] = min(99, player.attributes["Strength"] + strength_boost)
+    player.attributes["Vertical"] = min(99, player.attributes["Vertical"] + vertical_boost)
+    # Save the player
     return player
 
 
 # Changes the chance of an anomaly occurring
 # Each anomaly points to a function that applies the anomaly
-anomaly_chances = 0.02
+anomaly_chances = 1.00
 anomalies = {
     "Height": height_anomaly,
 }

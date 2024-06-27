bb357ca6bbab8ceb01cecc6cd5b583a0f09a012a
christopher.carlton <chriscarlton368@gmail.com>
2024-06-26 21:35:01 -0400

    Remove currency validation.

--- stafftools/views.py
+++ stafftools/views.py
@@ -21,9 +21,6 @@ def pay_user(request, id):
         currency = request.POST.get("pay-currency")
         reason = request.POST.get("pay-reason")
         _type = request.POST.get("pay-type")
-        # Validate the form data
-        if _type not in ["SP", "XP"]:
-            return HttpResponse(f"❌ Invalid currency type? {currency}")
         # Update the player's balance
         if amount and currency == "SP":
             amount = int(amount) if _type == "add" else (0 - abs(int(amount)))

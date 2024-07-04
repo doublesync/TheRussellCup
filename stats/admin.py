c14a965f1f8d201b2ca9269389ab3371fdc57409
christopher.carlton <chriscarlton368@gmail.com>
2024-07-04 13:45:59 -0400

    Updated admin

--- stats/admin.py
+++ stats/admin.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from unfold.admin import ModelAdmin
 
 # Local imports
-from stats.models import Game, TeamGameStats, PlayerGameStats
+from stats.models import Season, Game, TeamGameStats, PlayerGameStats
 
 # Register your models here.
 class PlayerGameStatsStatsAdmin(ModelAdmin):
     autocomplete_fields = ['player']
 
+admin.site.register(Season, ModelAdmin)
 admin.site.register(Game, ModelAdmin)
 admin.site.register(TeamGameStats, ModelAdmin)
 admin.site.register(PlayerGameStats, PlayerGameStatsStatsAdmin)

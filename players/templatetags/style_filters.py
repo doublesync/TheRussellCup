e49797e705435b5b2616a1e6f464fced1c102064
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-07-10 13:03:19 -0400

    Update style_filters.py

--- players/templatetags/style_filters.py
+++ players/templatetags/style_filters.py
@@ -12,6 +12,10 @@ register = template.Library()
 
 # This is a custom template tag that will be used to style the form fields in the create_player.html template.
 @register.filter
+def stringify(number):
+    return str(number)
+
+@register.filter
 def add_class(field, css):
     return field.as_widget(attrs={"class": css})
 

cc8f4a1b0472f4628b913e0576103f0e4b9e187d
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-07-11 19:02:44 +0000

    Statistics display skeleton

--- stats/urls.py
+++ stats/urls.py
@@ -7,9 +7,6 @@ from stats import views
 
 # Create your urls here.
 urlpatterns = [
-    # path('', login_required(views.stats_home), name='stats_home'),
-    # path('list/', login_required(views.stats_list), name='stats_list'),
-    path('players/<int:id>/', login_required(views.stats_player), name='stats_player'),
-    path('teams/<int:id>/', login_required(views.stats_teams), name='stats_teams'),
+    path("", views.stats_home, name="stats_home"),
     # path('season/<int:season_id>/', login_required(views.stats_season), name='stats_season'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -5,6 +5,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.generic.list import ListView
 
 # Local imports
 from django.views.generic import TemplateView
@@ -14,28 +15,13 @@ from teams.models import Team
 import simulation.statfinder as statfinder
 
 # Create your views here.
-
-def stats_player(request, id):
-    player = Player.objects.get(pk=id)
-    game_averages = statfinder.StatFinder().player_averages(player)
-    game_totals = statfinder.StatFinder().player_totals(player)
-    return JsonResponse({"Averages": game_averages, "Totals": game_totals})
-
-def stats_teams(request, id):
-    team = Team.objects.get(pk=id)
-    game_averages = statfinder.StatFinder(fetch_all_season=True).team_averages(team)
-    game_totals = statfinder.StatFinder(fetch_all_season=True).team_totals(team)
-    game_averages_dump = json.loads(json.dumps(game_averages, indent=4, sort_keys=True, default=str))
-    game_totals_dump = json.loads(json.dumps(game_totals, indent=4, sort_keys=True, default=str))
-    # we need these parameters in our jsonresponse : indent=4, sort_keys=True, default=str
-    return JsonResponse({"Averages": game_averages_dump, "Totals": game_totals_dump})
-    # Datetime field comes out as P0DT00H15M26S, why?
-
-# def stats_home(request):
-#     return HttpResponse("Stats Home")
-
-# def stats_list(request):
-#     return HttpResponse("Stats List")
+def stats_home(request):
+    objects = Player.objects.all()
+    players = {}
+    for player in objects:
+        players[player.id] = statfinder.StatFinder().player_averages(player)
+        players[player.id]["full_name"] = f"{player.first_name} {player.last_name}"
+    return render(request, "stats/stats_home.html", {"players": players})
 
 # def stats_season(request, id):
 #     return HttpResponse(f"S{id} Stats")

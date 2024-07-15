ea457148f141cc2abb04a7e3b9fe8f539b872951
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-07-15 19:15:00 +0000

    Cached player query results & centralized to function

--- stats/views.py
+++ stats/views.py
@@ -2,11 +2,12 @@
 import json
 
 # Django imports
-from django.shortcuts import render
+from django.core.cache import cache
 from django.http import HttpResponse
 from django.http import JsonResponse
-from django.views.generic.list import ListView
 from django.template.loader import render_to_string
+from django.shortcuts import render
+from django.views.generic.list import ListView
 
 # Local imports
 from django.views.generic import TemplateView
@@ -15,10 +16,18 @@ from players.models import Player
 from teams.models import Team
 import simulation.statfinder as statfinder
 
+def get_cached_players():
+    objects = cache.get("players")
+    if objects is None:
+        objects = Player.objects.all().only("id", "first_name", "last_name")
+        cache.set("players", objects, 300)
+    return objects
+
 # Create your views here.
 def stats_home(request):
-    objects = Player.objects.all()
+    objects = get_cached_players()
     players = {}
+    print(objects)
     for player in objects:
         players[player.id] = statfinder.StatFinder(fetch_all_season=True).player_averages(player)
         players[player.id]["full_name"] = f"{player.first_name} {player.last_name}"
@@ -26,13 +35,14 @@ def stats_home(request):
 
 # A function that sorts the players by a given stat
 def sort_by_stat(request, stat):
-    objects = Player.objects.all()
+    # Check cache for the players
+    objects = get_cached_players()
+    # Get the averages for each player
     players = {}
     for player in objects:
         players[player.id] = statfinder.StatFinder(fetch_all_season=True).player_averages(player)
         players[player.id]["full_name"] = f"{player.first_name} {player.last_name}"
-    # Sort by the stat & make a dictionary of players key: full_name, value: averages
-    # Order type can be ascending or descending
+    # Sort by the stat & make a dictionary of players
     order_type = request.GET.get("order-type")
     reverse_order = True if order_type == "asc" else False
     # Before sorting, set any None values to 0

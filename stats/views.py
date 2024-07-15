f0248c6f6902cba67503a6d4268cca682605d2b5
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-07-15 19:20:37 +0000

    Fixed order-type being null

--- stats/views.py
+++ stats/views.py
@@ -35,21 +35,22 @@ def stats_home(request):
 
 # A function that sorts the players by a given stat
 def sort_by_stat(request, stat):
-    # Check cache for the players
-    objects = get_cached_players()
-    # Get the averages for each player
-    players = {}
-    for player in objects:
-        players[player.id] = statfinder.StatFinder(fetch_all_season=True).player_averages(player)
-        players[player.id]["full_name"] = f"{player.first_name} {player.last_name}"
-    # Sort by the stat & make a dictionary of players
-    order_type = request.GET.get("order-type")
-    reverse_order = True if order_type == "asc" else False
-    # Before sorting, set any None values to 0
-    for player in players:
-        if players[player][stat] is None:
-            players[player][stat] = 0
-    sorted_players = {k: v for k, v in sorted(players.items(), key=lambda item: item[1][stat], reverse=reverse_order)}
-    # Render to string
-    fragment_html = render_to_string("stats/fragments/list_fragment.html", {"players": sorted_players})
-    return HttpResponse(fragment_html)
+    if request.method == "POST":
+        # Check cache for the players
+        objects = get_cached_players()
+        # Get the averages for each player
+        players = {}
+        for player in objects:
+            players[player.id] = statfinder.StatFinder(fetch_all_season=True).player_averages(player)
+            players[player.id]["full_name"] = f"{player.first_name} {player.last_name}"
+        # Sort by the stat & make a dictionary of players
+        order_type = request.POST.get("order-type")
+        reverse_order = True if order_type == "asc" else False
+        # Before sorting, set any None values to 0
+        for player in players:
+            if players[player][stat] is None:
+                players[player][stat] = 0
+        sorted_players = {k: v for k, v in sorted(players.items(), key=lambda item: item[1][stat], reverse=reverse_order)}
+        # Render to string
+        fragment_html = render_to_string("stats/fragments/list_fragment.html", {"players": sorted_players})
+        return HttpResponse(fragment_html)

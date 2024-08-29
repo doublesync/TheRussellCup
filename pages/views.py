a4cd7d8bb358800f6b2d4d137785495d5028ab16
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-08-29 18:17:27 +0000

    Added care package supporter portion to home page.

--- pages/views.py
+++ pages/views.py
@@ -10,8 +10,10 @@ from players.models import Player
 
 @login_required
 def home(request):
+    # Flat gives back single values: ('a', 'b', 'c') instead of tuples: [('a',), ('b',), ('c',)]
+    care_package_users = CustomUser.objects.filter(has_care_package=True).values_list("username", flat=True)
     user_players = Player.objects.filter(user=request.user)
-    return render(request, "pages/home.html", {"player_files": user_players})
+    return render(request, "pages/home.html", {"player_files": user_players, "care_package_users": care_package_users})
 
 def google_adsense(request):
     return HttpResponse("google.com, pub-4085265783135188, DIRECT, f08c47fec0942fa0")

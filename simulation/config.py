fa7ce9b92c79dbce2434c7645dd35baae77f83e9
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-09-01 11:07:16 -0400

    Update config.py

--- simulation/config.py
+++ simulation/config.py
@@ -8,8 +8,8 @@ CONFIG_USER = {
 # Really this is just for limiting how much a player can earn each 'game week'
 # It has no correlation to the seasons that will appear in the stats app
 CONFIG_SEASON = {
-    "CURRENT_WEEK": 11,
-    "GAME_WEEK": 11,
+    "CURRENT_WEEK": 12,
+    "GAME_WEEK": 12,
     "CURRENT_SEASON": 2,
     "MAX_SP_WEEK": 90,
     "MAX_SP_SEASON": 1620,

ed63316ae7ad1bd9d58cb74a80bac645c1f77546
christopher.carlton <chriscarlton368@gmail.com>
2024-09-04 20:19:48 -0400

    Moved game week.

--- simulation/config.py
+++ simulation/config.py
@@ -8,8 +8,8 @@ CONFIG_USER = {
 # Really this is just for limiting how much a player can earn each 'game week'
 # It has no correlation to the seasons that will appear in the stats app
 CONFIG_SEASON = {
-    "CURRENT_WEEK": 13,
-    "GAME_WEEK": 13,
+    "CURRENT_WEEK": 14,
+    "GAME_WEEK": 14,
     "CURRENT_SEASON": 2,
     "MAX_SP_WEEK": 90,
     "MAX_SP_SEASON": 1620,

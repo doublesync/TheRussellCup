fa9d3a61852401e2c90daec3c95682cb12b24f6a
Christopher Carlton <80734143+doublesync@users.noreply.github.com>
2024-09-12 11:04:37 -0400

    Update config.py

--- simulation/config.py
+++ simulation/config.py
@@ -8,19 +8,19 @@ CONFIG_USER = {
 # Really this is just for limiting how much a player can earn each 'game week'
 # It has no correlation to the seasons that will appear in the stats app
 CONFIG_SEASON = {
-    "CURRENT_WEEK": 18,
-    "GAME_WEEK": 18,
-    "CURRENT_SEASON": 2,
+    "CURRENT_WEEK": 0,
+    "GAME_WEEK": 0,
+    "CURRENT_SEASON": 3,
     "MAX_SP_WEEK": 90,
-    "MAX_SP_SEASON": 1620,
-    "CHECKIN_SP": 25,
-    "CHECKIN_XP": 75,
+    "MAX_SP_SEASON": 1800,
+    "CHECKIN_SP": 30,
+    "CHECKIN_XP": 100,
     "DEFAULT_CONTRACT": 150,
 }
 
 # Player configuration
 CONFIG_PLAYER = {
-    "SP_DEFAULT": 5000,
+    "SP_DEFAULT": 5750,
     "SP_ANOMALY_BONUS": 1500, # 30% of the starting SP
     "XP_DEFAULT": 8500,
     "BADGE_LABELS": {

9f4d8aa8691a7e964f266abbe3eef748400a6ae4
RCZach <46871233+RCZach@users.noreply.github.com>
2024-09-24 19:18:58 -0400

    Update attribute.py
    Updated attributes prices, names and sorting

--- simulation/scripts/attribute.py
+++ simulation/scripts/attribute.py
@@ -35,20 +35,20 @@ attribute_categories = {
         "Driving Layup",
         "Post Fade",
         "Post Hook",
-        "Post Moves",
+        "Post Control",
         "Draw Foul",
         "Close Shot",
-        "Midrange Shot",
-        "3pt Shot",
+        "Mid-Range Shot",
+        "Three-Point Shot",
         "Free Throw",
-        "Ball Control",
-        "Passing IQ",
+        "Ball Handle",
+        "Pass IQ",
         "Pass Accuracy",
         "Offensive Rebound",
         "Standing Dunk",
         "Driving Dunk",
         "Shot IQ",
-        "Passing Vision",
+        "Pass Vision",
         "Hands",
     ],
     "Defense": [
@@ -57,28 +57,34 @@ attribute_categories = {
         "Perimeter Defense",
         "Block",
         "Steal",
-        "Hustle", 
+    ],
+    "Physical": [
+        "Speed",
+        "Speed with Ball",
+        "Vertical",
+        "Strength", 
+        "Hustle"
+        "Agility"
     ],
     "Mental": [
         "Pass Perception",
         "Defensive Consistency",
         "Help Defense IQ",
         "Offensive Consistency",
-    ],
-    "Physical": physical_attributes,
+    ] 
 }
 
 # Attribute prices (in SP)
 attribute_prices = {
     real_range(55, 60): 1,
-    real_range(60, 65): 2,
-    real_range(66, 70): 4,
-    real_range(71, 75): 5,
+    real_range(60, 65): 3,
+    real_range(66, 70): 5,
+    real_range(71, 75): 7,
     real_range(76, 80): 10,
     real_range(81, 85): 20,
-    real_range(86, 90): 25,
-    real_range(91, 94): 30,
-    real_range(95, 99): 40,
+    real_range(86, 90): 30,
+    real_range(91, 94): 40,
+    real_range(95, 99): 50,
 }
 
 # Checks the price of an attribute and returns the cost

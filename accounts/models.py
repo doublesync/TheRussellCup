47ca595e1de10571d51378c754ccd7af82765b37
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-09-27 15:26:22 +0000

    Fixed model typo

--- accounts/models.py
+++ accounts/models.py
@@ -8,8 +8,6 @@ from django.db.models import ForeignKey
 
 # We need to add a field tot he User model to store the user's profile picture
 class CustomUser(AbstractUser):
-    sp = models.IntegerField(default=0)
-    xp = models.IntegerField(default=0)
     can_pay_players = models.BooleanField(default=False)
     can_mark_upgrades = models.BooleanField(default=False)
     has_care_package = models.BooleanField(default=False)

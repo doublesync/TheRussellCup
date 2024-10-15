97ded965681c792cbadcab26bef7938518122c26
Christopher Carlton <80734143+doublesync@users.noreply.github.com>
2024-10-15 11:07:25 -0400

    Added max anomaly heights.

--- simulation/scripts/anomaly.py
+++ simulation/scripts/anomaly.py
@@ -5,12 +5,22 @@ import random
 from accounts.models import CustomUser
 import simulation.config as config
 
+# Max anomaly heights
+max_anomaly_heights = {
+    "PG": 81,
+    "SG": 83,
+    "SF": 85,
+    "PF": 86,
+}
 
 # These are anomaly methods that are called when an anomaly is received
 def height_anomaly(player):
     # Initialize the anomaly effects
     player.anomaly = True
     player.height += random.randint(1, 3)
+    # Make sure height isn't over 'max_anomaly_heights' entry
+    if player.position in max_anomaly_heights and player.height > max_anomaly_heights[player.position]:
+        player.height = max_anomaly_heights[player.position]
     # Set the random boost values
     speed_boost = random.randint(5, 10)
     agility_boost = random.randint(5, 10)
@@ -28,7 +38,7 @@ def height_anomaly(player):
 
 # Changes the chance of an anomaly occurring
 # Each anomaly points to a function that applies the anomaly
-anomaly_chances = 0.015
+anomaly_chances = 0.01
 anomalies = {
     "Height": height_anomaly,
 }

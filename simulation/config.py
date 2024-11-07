bd91c94b5f5f3ad79dcac11907cb93056ffdd46c
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-11-07 19:46:09 +0000

    Update league configuration to season four.

--- simulation/config.py
+++ simulation/config.py
@@ -8,10 +8,10 @@ CONFIG_USER = {
 # Really this is just for limiting how much a player can earn each 'game week'
 # It has no correlation to the seasons that will appear in the stats app
 CONFIG_SEASON = {
-    "CURRENT_WEEK": 19,
-    "GAME_WEEK": 19,
-    "CURRENT_SEASON": 3,
-    "MAX_SP_WEEK": 90,
+    "CURRENT_WEEK": 0,
+    "GAME_WEEK": 0,
+    "CURRENT_SEASON": 4,
+    "MAX_SP_WEEK": 90, # Deprecated (scared to remove it!)
     "MAX_SP_SEASON": 2340, # Raised from 1800 SP
     "CHECKIN_SP": 30,
     "CHECKIN_XP": 100,

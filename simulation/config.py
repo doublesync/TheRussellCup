47cc0f59f2720eb1b14be985d7e84f22536ea0ab
RCZach <46871233+RCZach@users.noreply.github.com>
2024-11-16 09:55:43 -0500

    Update config.py
    temporarily remove surcharge

--- simulation/config.py
+++ simulation/config.py
@@ -34,7 +34,7 @@ CONFIG_PLAYER = {
     "SURCHARGE_STEP": 1000, # Every 1000 SP spent, the surcharge increases
     "SURCHARGE_INTEREST_INCREMENT": 0.02, # The surcharge interest rate increases by 2% for every 1000 SP spent
     "SURCHARGE_DEFAULT_INTEREST": 0.02, # The default surcharge interest rate
-    "SURCHARGE_THRESHOLD": 1000, # The threshold for when the surcharge interest starts to apply
+    "SURCHARGE_THRESHOLD": 100000, # The threshold for when the surcharge interest starts to apply
     "ROOKIE_CAP_HIT": 150, # The cap hit for a rookie player
 }
 

03eec2a4788f90685c6d644b882e227d39bed99f
doublesync <chriscarlton368@gmail.com>
2025-02-18 11:53:22 -0500

    Add scrolling game banner to home page

--- accounts/models.py
+++ accounts/models.py
@@ -10,8 +10,8 @@ from django.db.models import ForeignKey
 class CustomUser(AbstractUser):
     can_pay_players = models.BooleanField(default=False)
     can_mark_upgrades = models.BooleanField(default=False)
-    has_care_package = models.BooleanField(default=False)
-    has_second_player_slot = models.BooleanField(default=False)
+    has_care_package = models.BooleanField(default=False) # Now called a 'Season Pass': includes auto collect & modification discount
+    has_second_player_slot = models.BooleanField(default=False) # Grants access to a second player slot
 
     def __str__(self):
         return self.username
--- pages/views.py
+++ pages/views.py
@@ -6,14 +6,15 @@ from django.shortcuts import render
 # Local imports
 from accounts.models import CustomUser
 from players.models import Player
-
+from stats.models import Game
 
 @login_required
 def home(request):
     # Flat gives back single values: ('a', 'b', 'c') instead of tuples: [('a',), ('b',), ('c',)]
     care_package_users = CustomUser.objects.filter(has_care_package=True).values_list("username", flat=True)
     user_players = Player.objects.filter(user=request.user)
-    return render(request, "pages/home.html", {"player_files": user_players, "care_package_users": care_package_users})
+    recent_games = Game.objects.all().order_by("-created")[:20]
+    return render(request, "pages/home.html", {"player_files": user_players, "care_package_users": care_package_users, "recent_games": recent_games})
 
 def google_adsense(request):
     return HttpResponse("google.com, pub-4085265783135188, DIRECT, f08c47fec0942fa0")

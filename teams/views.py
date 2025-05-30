b4a30243ac223038ac6fba4c726894821917433f
doublesync <chriscarlton368@gmail.com>
2025-05-30 14:27:15 -0400

    Formatted views.py

--- teams/views.py
+++ teams/views.py
@@ -1,46 +1,49 @@
-# Python imports
-
-# Django imports
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
-
-# Local imports
-from teams.models import Team, Draft, DraftPick, DraftOrder
+import simulation.config as config
 import simulation.payment as payment
 import simulation.statfinder as statfinder
-import simulation.config as config
+from teams.models import Draft, DraftOrder, DraftPick, Team
+
 
 # Create your views here.
 def team_page(request, id):
-    # Initialize variables
     team = Team.objects.get(pk=id)
     salary_book = payment.get_salary_book(team)
     current_week = config.CONFIG_SEASON["CURRENT_WEEK"]
-    # Get the players' averages & totals
     stat_finder = statfinder.StatFinder()
     team_stats = stat_finder.team_player_stats(team)
-    # Loop through the players
-    return render(request, "teams/team_page.html", {
-        "team": team, 
-        "current_week": current_week, 
-        "salary_book": salary_book, 
-        "team_stats": team_stats, 
-        "rookie_cap_hit": config.CONFIG_PLAYER["ROOKIE_CAP_HIT"]
-    })
+    return render(
+        request,
+        "teams/team_page.html",
+        {
+            "team": team,
+            "current_week": current_week,
+            "salary_book": salary_book,
+            "team_stats": team_stats,
+            "rookie_cap_hit": config.CONFIG_PLAYER["ROOKIE_CAP_HIT"],
+        },
+    )
+
 
 def team_list(request):
     teams = Team.objects.all()
     return render(request, "teams/team_list.html", {"teams": teams})
 
+
 def draft_page(request, id):
     draft = Draft.objects.get(pk=id)
     draft_order = DraftOrder.objects.filter(draft=draft).order_by("order")
-    return render(request, "teams/draft_page.html", {"draft": draft, "draft_order": draft_order})
+    return render(
+        request, "teams/draft_page.html", {"draft": draft, "draft_order": draft_order}
+    )
+
 
 def draft_list(request):
     return HttpResponse("Work in progress...")
+
 
 class RosterMoveView(View):
 
@@ -51,4 +54,3 @@ class RosterMoveView(View):
 
     def post(self, request):
         return HttpResponse("POST request")
-    
